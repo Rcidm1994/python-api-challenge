ced9a15c07fd661c02ef91353cc970381328af62
Rcidm1994 <rodrigoalfredocidmoreno@gmail.com>
2021-02-13 19:02:39 -0600

    apis hw solved

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "46b729aae3ca48cef60a78c12937cb66"
+
+# Google API Key
+g_key = "AIzaSyCyORzqvlZ6GKN8vdg_mJ9gFy1cb52cfpU"
---
+++ WeatherPy/output_data/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "11d8fe6453ee07d19c3cec1993cc4dbe"
+
+# Google API Key
+g_key = "AIzaSyCyORzqvlZ6GKN8vdg_mJ9gFy1cb52cfpU"
